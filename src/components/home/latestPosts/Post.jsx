b508269313c35cd8abfec407c600c7b856488233
abdala <abdilahiabdirahman144@gmail.com>
2025-07-19 12:07:26 +0300

    tweaked the latest posts image

--- src/components/home/latestPosts/Post.jsx
+++ src/components/home/latestPosts/Post.jsx
@@ -1,9 +1,8 @@
 const Post = ({image}) => {
     return (
         <div className="bg-white shadow-2xl flex flex-col gap-1 hover:shadow-md transition-all duration-300 hover:-translate-z-1 max-w-70 sm:min-w-85 sm:max-w-120 mx-auto">
-            <div className="flex-1 max-w-full">
-                <img src={image} alt="" className="w-full flex-1 h-70 2xl:h-85" />
-            </div>
+            <div className="h-65 bg-cover bg-center" style={{ backgroundImage: `url(${image})` }}></div>
+
             <div className="flex-2 p-5 sm:p-10 flex flex-col gap-6">
                 <h3 className="text-2xl font-semibold">Borama Dam Project Overview</h3>
                 <p className="text-gray-600">By: abdilahiabdirahman144@gmail.com on january 3, 2025</p>
