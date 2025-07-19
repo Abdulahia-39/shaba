e80ab4ac56b9115a01d5eefdad71aca7555f761d
abdala <abdilahiabdirahman144@gmail.com>
2025-07-19 10:53:17 +0300

    tweaked the header section

--- src/components/home/header/Header.jsx
+++ src/components/home/header/Header.jsx
@@ -10,13 +10,13 @@ const Header = () => {
         >
             <ContactBar />
             <Navbar />
-            <div className="flex flex-col items-center justify-center text-white text-center h-3/4">
+            <div className="flex flex-col items-center justify-center text-white text-center min-h-3/4 max-h-2/3">
                 <h1 className='text-4xl/relaxed font-bold lg:text-6xl/relaxed w-3/4 mx-auto h-1/3 mb-5'>Shirkada adeegyada bulshada Awdal <span className='text-green-300'>SHABA</span></h1>
                 <p className='sm:text-xl text-lg text-wrap'>Shaba provides water services to borama’s community</p>
-                <div className="h-1/5 flex justify-center items-center">
+                <div className="h-1/5 flex justify-center items-center mb-5">
                     <button className='h-12 mt-10 border-2 border-white w-40 rounded-4xl hover:bg-green-300 hover:text-black hover:font-semibold transition duration-300'>Learn More</button>
                 </div>
-                <a href=''><i className='fas fa-chevron-down text-white text-2xl absolute bottom-5 animate-bounce cursor-pointer' 
+                <a href='' className='mt-5'><i className='fas fa-chevron-down text-white text-2xl absolute bottom-5 animate-bounce cursor-pointer' 
                     onClick={(e) => {
                     e.preventDefault();
                     window.scrollBy(0, window.innerHeight)}}>
